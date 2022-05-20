21ea676a032982d43c16faad19227e69ee5d1227
Johnny Black <lokedixon@hotmail.my>
2022-05-20 19:13:13 +0800

    Fixed crate series

--- tf2utilities/schema.py
+++ tf2utilities/schema.py
@@ -718,7 +718,7 @@ class Schema:
                     item["defindex"] = exclusiveGenuine[item['defindex']]
 
             if schemaItem["item_class"] == "supply_crate":
-                item["crateseries"] = self.getCrateSeriesList()[item["defindex"]]
+                item["crateseries"] = self.getCrateSeriesList()[str(item["defindex"])]
             
             elif schemaItem["item_class"] != "supply_crate" and number is not None:
                 item["craftnumber"] = number
