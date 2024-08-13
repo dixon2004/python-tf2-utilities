d8a54e640c3959dfa1cc7d838e6b68f88875f92a
Dixon <lokedixon@gmail.com>
2024-08-13 18:03:52 +0800

    Implemented retry logic for Steam API requests

--- tf2utilities/webapi.py
+++ tf2utilities/webapi.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 # Sends a request to the Steam API
@@ -6,12 +7,21 @@ def WebRequest(httpMethod, method, version, input):
 
     url = 'https://api.steampowered.com'
     face = 'IEconItems_440'
+    maxRetries = 5
 
     if httpMethod == "GET":
-        result = requests.get(f'{url}/{face}/{method}/{version}', params=input, timeout=10)
-        if result.status_code == 200 and result.json() is not None: 
-            return result.json()
-        else:
-            raise Exception('Steam API response: {}'.format(result.status_code))
+        for _ in range(maxRetries):
+            try:
+                response = requests.get(f'{url}/{face}/{method}/{version}', params=input, timeout=10)
+                response.raise_for_status()
+
+                result = response.json()
+                if not result:
+                    raise Exception(('Steam API returned an empty response.'))
+                
+                return result
+            except:
+                time.sleep(1)   
+                continue
     else:
         raise Exception(('Unknown Steam API http method.'))
