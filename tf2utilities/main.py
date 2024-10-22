2786d73d0558de4e968e1ba319feae959fe04021
vytal <80233205+SeanCole02@users.noreply.github.com>
2024-10-22 13:45:17 +0100

    Update main.py
    More robust handling of connection errors/loss on schema fetch

--- tf2utilities/main.py
+++ tf2utilities/main.py
@@ -1,3 +1,4 @@
+import requests.exceptions
 from tf2utilities.schema import Schema
 from threading import Thread
 import time
@@ -25,52 +26,56 @@ class TF2:
     # Gets the schema from the TF2 API
     def getSchema(self):
         if self.apiKey is not None:
-            raw = {
-                "schema": Schema.getOverview(self.apiKey) | {"items": Schema.getItems(self.apiKey), "paintkits": Schema.getPaintKits()},
-                "items_game": Schema.getItemsGame()
-            }
+            try:
+                while 1:
+                    raw = {
+                        "schema": Schema.getOverview(self.apiKey) | {"items": Schema.getItems(self.apiKey), "paintkits": Schema.getPaintKits()},
+                        "items_game": Schema.getItemsGame()
+                    }
+                    if self.lite:
+                        del raw["schema"]["originNames"]
+                        del raw["schema"]["item_sets"]
+                        del raw["schema"]["item_levels"]
+                        del raw["schema"]["string_lookups"]
 
-            if self.lite:
-                del raw["schema"]["originNames"]
-                del raw["schema"]["item_sets"]
-                del raw["schema"]["item_levels"]
-                del raw["schema"]["string_lookups"]
+                        del raw["items_game"]["game_info"]
+                        del raw["items_game"]["qualities"]
+                        del raw["items_game"]["colors"]
+                        del raw["items_game"]["rarities"]
+                        del raw["items_game"]["equip_regions_list"]
+                        del raw["items_game"]["equip_conflicts"]
+                        del raw["items_game"]["quest_objective_conditions"]
+                        del raw["items_game"]["item_series_types"]
+                        del raw["items_game"]["item_collections"]
+                        del raw["items_game"]["operations"]
+                        del raw["items_game"]["prefabs"]
+                        del raw["items_game"]["attributes"]
+                        del raw["items_game"]["item_criteria_templates"]
+                        del raw["items_game"]["random_attribute_templates"]
+                        del raw["items_game"]["lootlist_job_template_definitions"]
+                        del raw["items_game"]["item_sets"]
+                        del raw["items_game"]["client_loot_lists"]
+                        del raw["items_game"]["revolving_loot_lists"]
+                        del raw["items_game"]["recipes"]
+                        del raw["items_game"]["achievement_rewards"]
+                        del raw["items_game"]["attribute_controlled_attached_particles"]
+                        del raw["items_game"]["armory_data"]
+                        del raw["items_game"]["item_levels"]
+                        del raw["items_game"]["kill_eater_score_types"]
+                        del raw["items_game"]["mvm_maps"]
+                        del raw["items_game"]["mvm_tours"]
+                        del raw["items_game"]["matchmaking_categories"]
+                        del raw["items_game"]["maps"]
+                        del raw["items_game"]["master_maps_list"]
+                        del raw["items_game"]["steam_packages"]
+                        del raw["items_game"]["community_market_item_remaps"]
+                        del raw["items_game"]["war_definitions"]
 
-                del raw["items_game"]["game_info"]
-                del raw["items_game"]["qualities"]
-                del raw["items_game"]["colors"]
-                del raw["items_game"]["rarities"]
-                del raw["items_game"]["equip_regions_list"]
-                del raw["items_game"]["equip_conflicts"]
-                del raw["items_game"]["quest_objective_conditions"]
-                del raw["items_game"]["item_series_types"]
-                del raw["items_game"]["item_collections"]
-                del raw["items_game"]["operations"]
-                del raw["items_game"]["prefabs"]
-                del raw["items_game"]["attributes"]
-                del raw["items_game"]["item_criteria_templates"]
-                del raw["items_game"]["random_attribute_templates"]
-                del raw["items_game"]["lootlist_job_template_definitions"]
-                del raw["items_game"]["item_sets"]
-                del raw["items_game"]["client_loot_lists"]
-                del raw["items_game"]["revolving_loot_lists"]
-                del raw["items_game"]["recipes"]
-                del raw["items_game"]["achievement_rewards"]
-                del raw["items_game"]["attribute_controlled_attached_particles"]
-                del raw["items_game"]["armory_data"]
-                del raw["items_game"]["item_levels"]
-                del raw["items_game"]["kill_eater_score_types"]
-                del raw["items_game"]["mvm_maps"]
-                del raw["items_game"]["mvm_tours"]
-                del raw["items_game"]["matchmaking_categories"]
-                del raw["items_game"]["maps"]
-                del raw["items_game"]["master_maps_list"]
-                del raw["items_game"]["steam_packages"]
-                del raw["items_game"]["community_market_item_remaps"]
-                del raw["items_game"]["war_definitions"]
-
-            schema = {"time": time.time(), "raw": raw}
-            if isinstance(schema, dict): 
-                self.schema = Schema(schema)
-            else:
-                raise Exception("Schema is not a dict.")
+                    schema = {"time": time.time(), "raw": raw}
+                    if isinstance(schema, dict):
+                        self.schema = Schema(schema)
+                    else:
+                        raise Exception("Schema is not a dict.") 
+                    break
+            except requests.exceptions.ConnectionError:
+                time.sleep(10)
